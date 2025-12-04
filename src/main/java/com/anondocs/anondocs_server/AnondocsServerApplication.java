e4b889bd9050dbb9e865beb4c0f6495ca3636f2a
junhyukkkk <cjunk0304@gmail.com>
2025-12-04 22:54:43 +0900

    Init Project

---
+++ src/main/java/com/anondocs/anondocs_server/AnondocsServerApplication.java
@@ -0,0 +1,13 @@
+package com.anondocs.anondocs_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnondocsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnondocsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anondocs/anondocs_server/AnondocsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anondocs.anondocs_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnondocsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
